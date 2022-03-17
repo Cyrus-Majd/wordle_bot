ec612e635be7f914baa19118143eb1b30c4de69b
Cyrus Majd <cyrusmajd2001@gmail.com>
2022-03-16 22:45:11 -0400

    algorithm and letter distribution diagram

--- wordsFilter.py
+++ wordsFilter.py
@@ -1,3 +1,23 @@
+
+
+## the massive brain algorithm:
+
+# INPUT FOR FIRST ITERATION: nothing do a wild guess
+# 1st guess. come up with a random *completely diverse* word. so no repeating letters in the word.
+# the word should be skewed to the LEFT of the letter distribution diagram. So as to maximize chance of hitting correct letters.
+# OUTPUT OF FIRST GUESS: some letters not in the word, maybe some yellow, maybe some green
+
+# INPUT FOR SECOND ITERATION: the black, yellow, and green values.
+# 2nd guess. completely cross out (remove) all black letters from the word frequency distribution diagram.
+# take a guess with the constaints that there MUST BE greens in the correct spots and there MUST BE a yellow somewhere
+# OUTPUT OF SECOND GUESS: some letters not in the word, maybe some yellow, maybe some green
+
+# repeat this logic for all iterations.
+
+## SELECTION CRITERIA FOR PICKING A RANDOM WORD SKEWED TO THE LEFT: assign a "value" to each letter in the letter distribution diagram.
+# we add the values of each letter for each word and assign each word to a value. the highest value words mean that they contain the most left-skewing letters.
+# we then just add a constaint that says keep going until you find a completely diverse match.
+
 wordsOfCorrectLength = [] # here we go again with the long variable names nonsense
 
 # get rid of words that are not 5 letters long
@@ -8,7 +28,7 @@ def filterByLength():
         if (len(line) == 6):
             print(line)
             wordsOfCorrectLength.append(line)
-        
+
 
 # method to throw all words in a hashset for easy recalling.
 def hashWords():
@@ -20,6 +40,8 @@ def hashWords():
 def main():
     # first, get rid of everything that is not exactly 5 chars long.
     filterByLength()
+
+    
 
     english_words = hashWords()
     target = input("Enter the word you want to find in the dataset\n")
