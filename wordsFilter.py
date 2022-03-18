1066707e73bf65c23234bbc2ab34811df443f4b5
Cyrus Majd <cyrusmajd2001@gmail.com>
2022-03-17 23:58:35 -0400

    uhhh its not working :((

--- wordsFilter.py
+++ wordsFilter.py
@@ -18,7 +18,59 @@
 # we add the values of each letter for each word and assign each word to a value. the highest value words mean that they contain the most left-skewing letters.
 # we then just add a constaint that says keep going until you find a completely diverse match.
 
+# list of all words with length 5.
 wordsOfCorrectLength = [] # here we go again with the long variable names nonsense
+
+# map corresponding letters to frequency values.
+lettersValues = {
+    "E":12.02,
+    "T":9.10,
+    "A":8.12,
+    "O":7.68,
+    "I":7.31,
+    "N":6.95,
+    "S":6.28,
+    "R":6.02,
+    "H":5.92,
+    "D":4.32,
+    "L":3.98,
+    "U":2.88,
+    "C":2.71,
+    "M":2.61,
+    "F":2.30,
+    "Y":2.11,
+    "W":2.09,
+    "G":2.03,
+    "P":1.82,
+    "B":1.49,
+    "V":1.11,
+    "K":0.69,
+    "X":0.17,
+    "Q":0.11,
+    "J":0.10,
+    "Z":0.07,
+}
+
+# map corresponding each word to its value.
+wordValues = {}
+
+def valuePerWord(word):
+    sum = 0.0
+    print("type of sum", type(sum), "================")
+    for each in word:
+        print(each, "type of letters:", type(lettersValues.get(each.upper())) )
+        sum = sum + lettersValues.get(each.upper())
+        print(sum)
+    return sum
+
+# function to calculate the values of all words. puts results in wordValues map.
+def calculateAllValues():
+    for each in wordsOfCorrectLength:
+        print("calling")
+        type(valuePerWord(each))
+        # wordSum = valuePerWord(each)
+        print("setting")
+        # wordValues[each] = wordSum
 
 # get rid of words that are not 5 letters long
 def filterByLength():
@@ -26,7 +78,7 @@ def filterByLength():
     lines = wordFile.readlines()
     for line in lines:
         if (len(line) == 6):
-            print(line)
+            # print(line)
             wordsOfCorrectLength.append(line)
 
 
@@ -40,12 +92,16 @@ def hashWords():
 def main():
     # first, get rid of everything that is not exactly 5 chars long.
     filterByLength()
+    calculateAllValues()
 
+    # print(valuePerWord("AaA"))
+    # each = "Aa"
+    # print(lettersValues.get(each.upper()))
     
 
-    english_words = hashWords()
-    target = input("Enter the word you want to find in the dataset\n")
-    print(target in english_words)
+    # english_words = hashWords()
+    # target = input("Enter the word you want to find in the dataset\n")
+    # print(target in english_words)
 
 # fake main method because python is smart :)
 if __name__ == '__main__':
